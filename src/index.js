6f30b14ef0688f9c2e0c0f116e4aedf98c3e35f9
Andrew Bowers <andrew.bowers@agamecoder.com>
2023-07-08 08:42:45 -0700

    rearrange and start in on core functionality around text processing

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log('hello, wolrd!');
+import nlp from 'compromise/three';
+
+let doc = nlp('Wayne\'s World, party box time');
+let str = doc.match('#Person').out('array');
+console.log('hello, wolrd!', str);
