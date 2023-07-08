2efe222b820cd79ebe0e82f9d32d04c0a1331f69
Andrew Bowers <andrew.bowers@agamecoder.com>
2023-07-08 08:48:24 -0700

    Adding easier serving/watching for development

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import nlp from 'compromise/three';
 
 let doc = nlp('Wayne\'s World, party box time');
 let str = doc.match('#Person').out('array');
-console.log('hello, wolrd!', str);
+console.log('hello, world!', str);
